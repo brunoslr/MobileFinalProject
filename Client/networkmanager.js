6cb95f89880ad1307a26c681a68c150072b94dc2
Alex Herdzik <ajh2510@rit.edu>
2015-04-30 19:33:25 -0700

    Added handshake, sending and receiving position updates, and (theoretically, havent thoroughly tested) seeing other players moving around.

--- Client/networkmanager.js
+++ Client/networkmanager.js
@@ -1,8 +1,15 @@
+
+function NetworkedPlayer(playerObj, playerID){
+	this.obj= playerObj;
+	this.ID= playerID;
+};
+
 function NetworkManager(){
 	this.exampleSocket = "";
 	this.state = 0;
 	this.controls = "";
 	this.playerID= -1;
+	this.otherPlayerData= new Array();
 	this.update = function(controls){
 		//console.log(controls.getObject().x + " " + controls.getObject().y + " " + controls.getObject.z);
 		this.controls= controls;
@@ -10,13 +17,8 @@ function NetworkManager(){
 			case 0://socket has not yet been created
 				this.exampleSocket =new WebSocket("ws://127.0.0.1:8080");
 				this.state= 1;
-				this.exampleSocket.onopen = function (event) {
-					this.state= 2;
-				};
-				
-				this.exampleSocket.onmessage = function(event){
-					this.messageReceived(event);
-				};
+				this.exampleSocket.onopen =this.socketOnOpen.bind(this);
+				this.exampleSocket.onmessage = this.messageReceived.bind(this);
 			break;
 			case 2://socket has opened, and sent a welcome message, but the handshake message has not been received
 			
@@ -27,6 +29,11 @@ function NetworkManager(){
 		}
 	};
 	
+	this.socketOnOpen=function(event){
+		this.state= 2;
+		this.exampleSocket.send('id request');
+	};
+	
 	this.messageReceived = function(event){
 		var splitString =event.data.split("\n");
 		switch(splitString[0]){
@@ -34,23 +41,58 @@ function NetworkManager(){
 				if(this.state ==2 ){
 					this.state= 3;
 					this.playerID= parseInt(splitString[1]);
+					console.log("id request: " + this.playerID);
 				}
 			break;
 			
 			case "position update":
+				for(var i=1; i<splitString.length-1; i+=4){
+					var curPlayerID= parseInt(splitString[i]);
+					
+					if(curPlayerID == this.playerID){
+						continue;
+					}
+					if(this.otherPlayerData[curPlayerID]==null){
+						var geometry = new THREE.BoxGeometry( 10, 20, 10 );
+						for ( var l = 0, l = geometry.faces.length; l < l; l ++ ) {
+
+							var face = geometry.faces[ l ];
+							face.vertexColors[ 0 ] = new THREE.Color( 1,0,0 );
+							face.vertexColors[ 1 ] = new THREE.Color( 1,0,0);
+							face.vertexColors[ 2 ] = new THREE.Color( 1,0,0 );
+
+						}
+						var material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } );
+						var mesh = new THREE.Mesh( geometry, material );
+						mesh.position.x = parseInt(splitString[i+1]);
+						mesh.position.y = parseInt(splitString[i+2]);
+						mesh.position.z = parseInt(splitString[i+3]);
+						scene.add( mesh );
+						var newPlayer= new NetworkedPlayer(curPlayerID,mesh);
+						this.otherPlayerData[curPlayerID]=newPlayer;
+						material.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+					}
+					else{
+						this.otherPlayerData[curPlayerID].obj.position.x= parseInt(splitString[i+1]);
+						this.otherPlayerData[curPlayerID].obj.position.y= parseInt(splitString[i+2]);
+						this.otherPlayerData[curPlayerID].obj.position.z= parseInt(splitString[i+3]);
+					}
+				}
 			break;
 		}
 	};
 	
 	this.updateServer = function(event){
 		var updateString = "position update\n";
-		updateString.concat(controls.getObject().position.x + "\n");
-		updateString.concat(controls.getObject().position.y + "\n");
-		updateString.concat(controls.getObject().position.z + "\n");
+		updateString.concat(this.playerID + "\n");
+		updateString.concat(this.controls.getObject().position.x + "\n");
+		updateString.concat(this.controls.getObject().position.y + "\n");
+		updateString.concat(this.controls.getObject().position.z + "\n");
+		this.exampleSocket.send(updateString);
 	};
 	
 	this.sendBulletData = function (bullet){
 		
 	}
 	
-}
+};
--- Server/index.js
+++ Server/index.js
@@ -1,27 +1,53 @@
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: 8080 });
+var IDIterator= 0;
+var wsArray = new Array();
 
-function WebConnection(con,Name){
+function WebConnection(con){
 	this.connection=con;
-	this.name=Name;
-}
-
-var wsArray= new Array();
+	this.position = {x:0, y:0, z:0};
+	this.ID= IDIterator;
+	IDIterator++;
+};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-	console.log("TOTALMESSAGE");
-	console.log(message);
-	console.log("ENDTOTALMESSAGE");
-	/*var splitArray= message.split("\n");
-	switch(parseInt(splitArray[0])){
-		case 1:
-		wsArray.push(new WebConnection(ws,splitArray[1]));
+
+	var splitArray= message.split("\n");
+	switch(splitArray[0]){
+		case "id request"://handshake message
+		var newConnection = new WebConnection(ws);
+		wsArray.push(newConnection);
+		newConnection.connection.send("handshake\n" + newConnection.ID);
 		break;
+		case "position update":
+			for(var i= 0; i<wsArray.length; i++){
+				if(wsArray[i].ID==splitArray[1]){
+					wsArray.x= parseInt(splitArray[2]);
+					wsArray.y= parseInt(splitArray[3]);
+					wsArray.z= parseInt(splitArray[4]);
+				}
+			}
+		break;
+		
 		default:
 		break;
-	}*/
+	}
   });
-  console.log("connected");
   
 });
+
+var timer= setInterval(function() { 
+	var positionUpdate = "position update\n";
+	for(var i=0; i<wsArray.length; i++){
+		positionUpdate.concat(wsArray[i].ID + "\n");
+		positionUpdate.concat(wsArray[i].position.x + "\n");
+		positionUpdate.concat(wsArray[i].position.y + "\n");
+		positionUpdate.concat(wsArray[i].position.z + "\n");
+	}
+	for(var i=0; i<wsArray.length; i++){
+		wsArray[i].connection.send(positionUpdate);
+	}
+},1000/30);
+
+
